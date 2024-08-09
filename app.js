d595939bdc7f92f94e5eba6a44dd1517ed817893
sadyreis <sadyvitoriareissilva06@gmail.com>
2024-08-09 14:48:47 -0300

    app_clima

---
+++ app.js
@@ -0,0 +1,16 @@
+//Variáveis e seleção de elementos
+const apiKey = "2da0366edf274b259a200b4a8f4e9ee7";
+const apiCountryURL = "https:countryflagsapi.netlify.app/flag/BR.svg";
+
+const cityInput = document.querySelector("city-input");
+const searchBtn = document.querySelector("search");
+
+//Funções
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log("teste");
+})
+
+
+//Eventos
